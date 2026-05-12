f5ecb2957ca5552e271f2a9a039f9dcb29776eb5
hayeon000000 <gkdus7002@gmail.com>
2026-05-12 19:04:01 +0900

    fix: BrowserRouter 추가해서 까만 화면 해결

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css'; // 테일윈드나 기본 CSS 설정
+import { BrowserRouter } from 'react-router-dom'; // 🌟 무대(라우터) 불러오기!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* 🌟 App 전체를 BrowserRouter 무대로 감싸줍니다! */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
